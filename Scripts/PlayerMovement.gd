edffe48259a6d9861c6f19690cd0cbcae8d09029
DESKTOP-H7HA8VG\dakkua <daniel.guerra.gallardo99@gmail.com>
2024-06-11 13:10:42 +0200

    feat: added cyclops level builder pluging + basic player jump (wip)

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,14 +3,28 @@ extends RigidBody3D
 @export var movement_speed : float = 385.0
 @export var max_speed : float = 7.5
 
-@onready var camera_3d = $"../Camera3D"
+@export var jump_force : float = 35.0
+@export var gravity : float = -9.81
+
+var vertical_velocity : float = 0.0
+
+@onready var camera_3d : Camera3D = $"../Camera3D"
+@onready var ray_cast_3d : RayCast3D = $"../RayCast3D"
+
+
+func _ready() -> void:
+	ray_cast_3d.exclude_parent = true
+	
+	# todo: bloquear rotación en los ejes
+
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
+	jump(delta)
 	
 # Player physical movement
 # params: delta: float - time between frames
-func movement(delta) -> void:
+func movement(delta : float) -> void:
 	
 	# Get the input from the player
 	var f_input: float = Input.get_action_strength("backward") - Input.get_action_strength("forward")
@@ -31,5 +45,26 @@ func movement(delta) -> void:
 	apply_central_force(direction_f * movement_speed * delta)
 	apply_central_force(direction_h * movement_speed * delta)
 
+# Player jump
+# params: delta: float - time between frames
+func jump(delta : float) -> void:
+	
+	# Apply gravity
+	vertical_velocity += gravity * delta
 
+	# Check if the player has pressed the jump button
+	if Input.is_action_just_pressed("jump"):
+		vertical_velocity = jump_force
+		
+	#Check if the player is on ground
+	if not (is_on_ground()):
+		vertical_velocity = 0
+		
+	vertical_velocity += gravity * delta
+
+	# Apply the vertical velocity to the player
+	apply_central_force(Vector3(0, vertical_velocity, 0))	
+
+func is_on_ground() -> bool:
+	return ray_cast_3d.is_colliding()
 
