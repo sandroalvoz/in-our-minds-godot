baa4913a894eee7bd82e21ec5d0573e00f83408c
DESKTOP-H7HA8VG\dakkua <daniel.guerra.gallardo99@gmail.com>
2024-06-15 13:08:00 +0200

    feat: raycast finally detect correctly floor body.

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+var LineDrawer = preload("res://Scripts/DrawLine3D.gd").new()
+
 @export var movement_speed : float = 385.0
 @export var max_speed : float = 7.5
 
@@ -7,11 +9,14 @@ extends RigidBody3D
 @export var gravity : float = -9.81
 
 var vertical_velocity : float = 0.0
+@export var ray_cast_3d_yOffset : float = 2;
 
 @onready var camera_3d : Camera3D = $"../Camera3D"
-@onready var ray_cast_3d = $"../RayCast3D"
+@onready var ray_cast_3d = $"RayCast3D"
 
-   
+
+func _ready() -> void:
+	add_child(LineDrawer)
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
@@ -44,6 +49,12 @@ func movement(delta : float) -> void:
 # params: delta: float - time between frames
 func jump(delta : float) -> void:
 	
+	# Move manually raycast position
+	ray_cast_3d.position = Vector3(
+		position.x, 
+		position.y + ray_cast_3d_yOffset, 
+		position.z)
+
 	# Apply gravity
 	vertical_velocity += gravity * delta
 
@@ -58,7 +69,17 @@ func jump(delta : float) -> void:
 		apply_central_force(Vector3(0, vertical_velocity, 0))	
 	
 
+# Check if player is on ground 
 func is_on_ground() -> bool:
+	
+	# Draw raycast line (debug)
+	LineDrawer.DrawLine(
+		ray_cast_3d.position, 
+		ray_cast_3d.position + ray_cast_3d.target_position, 
+		Color.RED
+		)
+	
+	# Check raycast
 	if(ray_cast_3d.is_colliding()):
 		return true
 	else:
