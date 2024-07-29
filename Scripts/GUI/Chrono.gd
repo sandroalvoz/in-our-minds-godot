06c78d1206a9b61c178b5dbf2626740f5a9e456e
Daniel G. <61831013+DakkuaDev@users.noreply.github.com>
2024-07-29 14:23:37 +0200

    Added Description to GUI and basic event interaction with player

--- Scripts/GUI/Chrono.gd
+++ Scripts/GUI/Chrono.gd
@@ -1,14 +1,14 @@
 extends PanelContainer
 
-@onready var label = $Label
+@onready var chrono_label = $ChronoLabel
 var elapsed_time: float = 0.0
 
 func _ready():
-	label.text = "00:00"
+	chrono_label.text = "00:00"
 
 func _process(delta: float):
 	elapsed_time += delta
-	label.text = format_time(elapsed_time)
+	chrono_label.text = format_time(elapsed_time)
 	
 func format_time(time: float) -> String:
 	var minutes = int(int(time / 60) % 60)
--- Scripts/GUI/GUIManager.gd
+++ Scripts/GUI/GUIManager.gd
@@ -1,3 +1,14 @@
 extends Control
 
-@export var scene_name : String
+@onready var level_label = $LevelPanel/LevelLabel
+@onready var description_label = $DescriptionPanel/DescriptionLabel
+@onready var description_panel = $DescriptionPanel
+
+
+func _ready():
+	var scene_number = str(SceneManager._current_scene)
+	level_label.text = scene_number + "\n" + "scene_name"
+
+func on_show_description(messg : String) -> void :
+	description_panel.visible = true
+	description_label.text = messg 
