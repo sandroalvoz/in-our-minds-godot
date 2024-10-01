98d581f7d2d87e4d22d6700455c35bcb4e8bcc36
daniel.guerra <daniel.guerra.gallardo99@gmail.com>
2024-10-01 15:57:00 +0200

    feat: language selection popup added to Main Menu options button (es-en)

--- Scripts/GUI/ChapterMenu.gd
+++ Scripts/GUI/ChapterMenu.gd
@@ -1,4 +1,19 @@
 extends Control
 
+@onready var btn_start = $Panel/VBoxContainer/BtnStart
+@onready var timer = $Timer
+
+func _ready():
+	timer.start(5)
+	btn_start.visible = false
+
 func _on_btn_start_button_down():
 	GameManager._start_game()
+	
+	pass
+
+func _on_timer_timeout():
+	timer.stop()
+	btn_start.visible = true
+	
+	pass
--- Scripts/GUI/MainMenu.gd
+++ Scripts/GUI/MainMenu.gd
@@ -2,6 +2,13 @@ extends Control
 
 @onready var btn_start : Button = $Panel/VBoxContainer/BtnStart
 @onready var btn_exit : Button = $Panel/VBoxContainer/BtnExit
+@onready var btn_options = $Panel/VBoxContainer/BtnOptions
+
+@onready var popup_menu = $Panel/VBoxContainer/BtnOptions/PopupMenu
+
+
+func _ready():
+	popup_menu.hide()
 
 func _on_btn_start_button_down() -> void:
 	GameManager._on_show_chapter_menu()
@@ -12,3 +19,14 @@ func _on_btn_exit_button_down() -> void:
 	GameManager._exit_game()
 	
 	pass
+
+func _on_btn_options_button_down() -> void:
+	popup_menu.popup()
+	
+	pass 
+
+func _on_popup_menu_index_pressed(index) -> void:
+	match index:
+		1: TranslationServer.set_locale("en")
+		2: TranslationServer.set_locale("es") 
+	pass 
