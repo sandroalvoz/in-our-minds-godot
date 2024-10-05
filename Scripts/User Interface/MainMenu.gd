7257b9b71958d9a9e1577d82c9252beb45bd16e2
daniel.guerra <daniel.guerra.gallardo99@gmail.com>
2024-10-05 20:34:23 +0200

    fix: screenmode option

--- Scripts/User Interface/MainMenu.gd
+++ Scripts/User Interface/MainMenu.gd
@@ -9,14 +9,16 @@ extends Control
 
 const FULL_SCREEN_INDEX : int = 4
 
+func _ready():
+	popup_menu.hide()	
+	_on_fullscreen()
+	
 func _process(delta):
 	#DEBUG MODE
 	if Input.is_action_just_pressed("DEBUG_MODE"):
 		debug_text_edit.visible = true
-
-func _ready():
-	popup_menu.hide()
-	_on_fullscreen()
+		
+	pass
 
 func _on_btn_start_button_down() -> void:
 	GameManager._on_show_chapter_menu()
@@ -42,12 +44,16 @@ func _on_popup_menu_id_pressed(id) -> void:
 		pass
 		
 func _on_fullscreen() -> void:
-	if(GameManager.full_screen_mode):
+	if(!GameManager.full_screen_mode):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		popup_menu.set_item_checked(FULL_SCREEN_INDEX, true)		
+		
+		GameManager.full_screen_mode = true
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		popup_menu.set_item_checked(FULL_SCREEN_INDEX, false)
+		
+		GameManager.full_screen_mode = false
 
 	!GameManager.full_screen_mode
 
